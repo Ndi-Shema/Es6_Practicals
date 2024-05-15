968d35e1806da57845d7aec4f44d61790d106f60
Ndi-Shema <f.shema@alustudent.com>
2024-05-15 13:48:26 +0200

    .startWith

--- newStringMethods.js
+++ newStringMethods.js
@@ -5,3 +5,16 @@
 
 // All();
 
+// function All(){
+//     let str = "goodbye";
+//     console.log(str.startsWith("good"));
+// }
+
+// All();
+
+function All(){
+    let str = "graayyy";
+    console.log(str.)
+}
+
+All();
