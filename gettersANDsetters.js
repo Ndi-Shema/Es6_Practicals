80bfbf535a5361448f540c622c2d933e23bbc961
Ndi-Shema <f.shema@alustudent.com>
2024-05-19 20:44:16 +0200

    done with the setters and getters

--- gettersANDsetters.js
+++ gettersANDsetters.js
@@ -3,20 +3,27 @@ class square {
     constructor (_width) {
         this.width = _width;
         this.height = _width;
+        this.numOfRequestForArea = 0;
     }
 
     get area () {
+        this.numOfRequestForArea++;
         return this.width * this.height;
     }
 
     set area (area){
         this.width = Math.sqrt(area);
         this.height = this.width;
+        
     }
 }
 
 let square1 = new square(4);
 square1.area = 25;
 console.log(square1.area);
-console.log(square1.width);
-console.log(square1.height);
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+
+console.log(square1.numOfRequestForArea);
