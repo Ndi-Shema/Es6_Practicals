c03a4273e34650aa63f6ee6dc85b52e74154c541
Ndi-Shema <f.shema@alustudent.com>
2024-05-19 14:50:42 +0200

    arrow keys

---
+++ arrowKeyFunction.js
@@ -0,0 +1,19 @@
+// little bit of notes 
+
+// let add = (x, y)=>{
+//     return x+y;
+// }
+
+// console.log(add(2, 78));
+
+// let numbers = [4,2,6];
+// numbers.sort((a,b) => a - b);
+// console.log(numbers);
+
+ // ARROW FUNCTIONS
+
+ let ninjaGreetings = name => console.log(`${name} hiiiiyaa`);
+
+ ninjaGreetings("mark")
+
+//  console.log(ninjaGreetings);
